bf98b7cc48e53b79a5f657cd057a3753b18d33d9
worm <worm@worm.worm>
2024-05-24 21:29:24 +0300

    Frontend working

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #local apps
+    'pages',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,6 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
---
+++ app/pages/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    context = {
+        'title': 'Home'
+    }
+    return render(request, 'pages/home.html', context)
+
+def about(request):
+    context = {
+        'title': 'About'
+    }
+    return render(request, 'pages/about.html', context)
+
+def services(request):
+    context = {
+        'title': 'Services'
+    }
+    return render(request, 'pages/services.html', context)
+
+def contact(request):
+    context = {
+        'title': 'Contact'
+    }
+    return render(request, 'pages/contact.html', context)
